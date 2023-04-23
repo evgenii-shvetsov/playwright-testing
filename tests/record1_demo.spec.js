687484f40eeb2964faa01c3d7b792c60029e4d21
evgenii-shvetsov <sea.dev91@gmail.com>
2023-04-22 18:52:00 -0700

    add tracing

--- tests/record1_demo.spec.js
+++ tests/record1_demo.spec.js
@@ -6,7 +6,10 @@ test("record demo test", async ({ page }) => {
   await page.locator('[data-test="username"]').fill("standard_user");
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill("secret_sauce");
+
   await page.locator('[data-test="login-button"]').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+
   await page.getByRole("button", { name: "Open Menu" }).click();
   await page.getByRole("link", { name: "Logout" }).click();
 });
