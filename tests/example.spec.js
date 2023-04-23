71e2f9d29aaf64e02bd0db1b74704563da05182a
evgenii-shvetsov <sea.dev91@gmail.com>
2023-04-22 19:15:39 -0700

    add tracing

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,26 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page, context }) => {
+  await context.tracing.start({
+    snapshots: true,
+    screenshots: true,
+  });
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await context.tracing.stop({ path: "test1_trace.zip" });
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
+
+  
 });
